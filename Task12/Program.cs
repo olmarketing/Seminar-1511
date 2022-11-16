4ec8e5448a9f052dfe4e009db9cb1462fd2666a9
Anastasiya <dolivia84@mail.ru>
2022-11-16 11:27:58 +0500

    add Task12 2 metods

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -6,35 +6,24 @@
 // 16, 4 -> кратно
 
 
-// Console.WriteLine("Первое число - ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Второе число - ");
-// int b = Convert.ToInt32(Console.ReadLine());
-
-// void Multiply(int numb1, int numb2)
-// {
-//     if (numb1 % numb2 == 0 )
-//     {
-//         Console.WriteLine($"Число {numb1} кратное числу {numb2}");
-//     }
-//     else
-//     Console.WriteLine($"Остаток от деления {numb1} на число {numb2} = {numb1 % numb2}");
-   
-// }
-
-// Multiply(a,b);
-
-
-Console.WriteLine("Первое число - ");
+Console.WriteLine("Введите первое число - ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Второе число - ");
+Console.WriteLine("Введите второе число - ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-bool Multiply(int numb1, int numb2)
+// bool Multiply(int numb1, int numb2)
+// {
+//     if (numb1 % numb2 == 0) return true;
+//     return false;
+// }
+
+// bool result = Multiply(a, b);
+// Console.WriteLine(result ? "Кратно" : $"Некратно, остаток = {a % b}");
+
+int Multiply(int numb1, int numb2)
 {
-    if (numb1 % numb2 == 0 ) return true;
-    return false;
+    return numb1 % numb2;
 }
 
-bool result  = Multiply(a,b);
-Console.WriteLine(result ? "Кратно" : $"Некратно, остаток = {a % b}");
+int result = Multiply(a, b);
+Console.WriteLine(result == 0 ? "Кратно" : $"Некратно, остаток = {result}");
