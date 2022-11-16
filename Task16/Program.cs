b511b2f0f92b20931e7d01b491b7a93feb6d1eab
Anastasiya <dolivia84@mail.ru>
2022-11-16 13:09:32 +0500

    Add Task 16

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -6,3 +6,14 @@
 // 25, 5 -> да
 // 8,9 -> нет
 
+Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool Square(int number1, int number2)
+{
+    return (number1 % number2 == 0) || (number2 % number1 == 0);
+}
+
+Console.WriteLine(Square(num1, num2) ? " Да" : "Нет");
