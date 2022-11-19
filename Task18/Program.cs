cc3643bce4e45d0c64f6cc3edacac72f8d2243b8
Anastasiya <dolivia84@mail.ru>
2022-11-19 12:25:45 +0500

    Add + Task 18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -2,15 +2,15 @@
 // показывает диапазон возможных координат точек в этой четверти (x и y)
 
 Console.WriteLine("Введите номер четверти: ");
-int number = Convert.ToInt32(Console.ReadLine());
+string number = Console.ReadLine();
 
 
-string Coord(int num)
+string Coord(string num)
 {
-    if (num == 1) return "x > 0 y > 0";
-    if (num == 2) return "x < 0 y > 0";
-    if (num == 3) return "x < 0 y < 0";
-    if (num == 4) return "x > 0 y > 0";
+    if (num == "1") return "x > 0 y > 0";
+    if (num == "2") return "x < 0 y > 0";
+    if (num == "3") return "x < 0 y < 0";
+    if (num == "4") return "x > 0 y > 0";
     return "Некорректное значение";
 
 }
