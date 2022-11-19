347bc891856ca0958528d95aab77ef015456b8a6
Anastasiya <dolivia84@mail.ru>
2022-11-19 13:46:03 +0500

    Add Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает таблицу квадратов чисел от 1 до N.
+
+int number = -1;
+while (number < 1)
+{
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+if (number<1) Console.Writeline("Введите положительное значение");
+}
+
+void Square(int num)
+{
+
+    // int i = 1;
+    // while (i<=num);
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine($"{i} -> {i*i}");
+    //i++;
+}
+}
+
+Square(number);
