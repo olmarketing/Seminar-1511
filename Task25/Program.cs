5ae3f60ee635b0a0133bff9e06dd9a60fa2479e7
Anastasiya <dolivia84@mail.ru>
2022-11-22 15:29:58 +0500

    Add Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b < 0)
+{
+    Console.WriteLine($"Число {b} не натуральное");
+}
+else
+{
+    int Degree(int a1, int b1)
+    {
+        int res = a1;
+        for (int i = 1; i < b1; i++)
+        {
+            res = res * a1;
+        }
+        return res;
+    }
+    int degree = Degree(a, b);
+    Console.WriteLine($"{a} в степени {b} равно {degree}");
+}
---
+++ Task28/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Multiplier(int num)
+{
+
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+
+if (number > 0)
+{
+    int multiplier = Multiplier(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {multiplier}");
+
+}
+
+else Console.WriteLine("Требуется внести натуральное число");
