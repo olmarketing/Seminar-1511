3e424c72c9ffde451edafa4e03903cfd5ec69912
Anastasiya <dolivia84@mail.ru>
2022-12-04 21:54:20 +0300

    Add Task42

---
+++ Task40/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и проверяет, может ли существовать треугольник
+// со сторонами такой длины.
+// Каждая сторона треугольника меньше
+// суммы двух других сторон.
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle(int a1, int b1, int c1)
+{
+    return a1 < b1+c1 && b1< a1+c1 && c1< a1+b1;
+}
+
+Console.WriteLine(Triangle(a,b,c) ? 
+"Такой треугольник существует" 
+: "Такого треугольника не существует");
+
+
---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+// 45->101101
+// 3-> 11
+// 2->10
+
+string Binary(int numb)
+{
+    string bin = string.Empty;
+    while (numb> 0) 
+    {
+        bin = numb % 2 + bin; 
+        numb /= 2; 
+    }
+    return bin; 
+
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Binary(number));
